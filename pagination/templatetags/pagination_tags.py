a1882292ef85f2b5aab17aa91e43ebc301d07fa7
q <corey@qr7.com>
2011-03-16 02:13:04 -0400

    helps if i actually test _my_ code. wow. such fail.

--- pagination/templatetags/pagination_tags.py
+++ pagination/templatetags/pagination_tags.py
@@ -257,13 +257,12 @@ class PaginateWithTemplate(template.Node):
         render_context = paginate(context)
 
         rendered = render_to_string(template_list, render_context)
-        return ''
+        return rendered
 
 @register.tag(name='paginate_template')
 def do_paginate_with_template(parser, token):
     try:
         tag_name, template_path = token.split_contents()
-        print template_path
     except ValueError:
         raise template.TemplateSyntaxError, "%r tag requires exactly two arguments" % token.contents.split()[0]
     return PaginateWithTemplate(template_path, **args)
